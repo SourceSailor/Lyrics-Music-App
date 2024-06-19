f4358efd832b54ead10da92825429639390111a9
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-06-19 16:30:53 -0700

    Added Related Songs Component to Artist Details

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { useGetArtistsDetailsQuery } from "../redux/services/shazamCore";
+import { Link } from "react-router-dom";
+import PlayPause from "../components/PlayPause";
 
 const ArtistDetails = ({ delay }) => {
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const [delayed, setDelayed] = useState(false);
 
-  // Setting a Timeout to delayed state using a prop from Song Details
   useEffect(() => {
     const timer = setTimeout(() => {
       setDelayed(true);
@@ -18,40 +19,105 @@ const ArtistDetails = ({ delay }) => {
     return () => clearTimeout(timer);
   }, [delay]);
 
-  // Creating conditional logic, fetching the data from the API to execute after setDelayed and songid is truthy
   const {
     data: artistData,
     isFetching,
     error,
-  } = useGetArtistsDetailsQuery(artistId, { skip: !delayed });
+  } = useGetArtistsDetailsQuery(artistId, {
+    skip: !delayed,
+  });
 
   if (isFetching) return <Loader title="Loading artist details..." />;
   if (error) return <Error />;
-  const artistDataBoilerPlate = artistData?.data?.[0];
 
-  console.log(
-    "Artist Data From Artist Details Component: ",
-    artistDataBoilerPlate
-  );
+  const artistDataBoilerPlate = artistData?.data?.[0];
+  const artistRelatedSongsDataBoilerPlate =
+    artistData?.data?.[0]?.views["top-songs"]?.data;
 
   return (
     <div className="flex flex-col">
+      {/* Artist Details Header */}
+
       <DetailsHeader artistId={artistId} artistData={artistData} />
+
+      {/* Artist Bio */}
       <div className="mt-10 mb-3">
         <h2 className="text-white text-3xl font-bold">Bio:</h2>
-        <p className="text-white text-lg my-5">
-          {artistDataBoilerPlate?.attributes?.artistBio}
-        </p>
+        {artistDataBoilerPlate?.attributes?.artistBio ? (
+          <p className="text-white text-lg my-5">
+            {artistDataBoilerPlate?.attributes?.artistBio}
+          </p>
+        ) : (
+          <p className="text-white text-xl mt-5">Sorry, no bio found...</p>
+        )}
       </div>
 
-      {/* Related Songs Component */}
-      <RelatedSongs
-        delay={3000}
-        // data={Object.values(artistData?.song)}
-        artistId={artistId}
+      {/* Artists Related Songs */}
+      <div className="mt-10 mb-3">
+        <h2 className="text-white text-3xl font-bold mb-5">Related Songs:</h2>
+
+        {artistRelatedSongsDataBoilerPlate ? (
+          artistRelatedSongsDataBoilerPlate.map((song, i) => (
+            <TopArtistRelatedSongs
+              key={song.id}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              artistId={artistId}
+            />
+          ))
+        ) : (
+          <p className="text-white">No songs found.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const TopArtistRelatedSongs = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => {
+  console.log("asfsdfsdfsda: ", song);
+  return (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      {/* Counter Numeral */}
+      <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <img
+          className="w-20 h-20 rounded-lg"
+          src={song?.attributes?.artwork?.url}
+          alt={song?.attributes?.name}
+        />
+
+        <div className="flex-1 flex flex-col justify-center mx-3">
+          {/* Song Title */}
+
+          <Link to={`/songs/${song?.id}`}>
+            <p className="text-xl font-bold text-white">
+              {song?.attributes?.name}
+            </p>
+          </Link>
+
+          {/* Song Artist */}
+          <Link to={`/artists/${song?.relationships?.artists?.data[0]?.id}`}>
+            <p className="text-base text-gray-300 mt-1">
+              {song?.attributes?.artistName}
+            </p>
+          </Link>
+        </div>
+      </div>
+      <PlayPause
         isPlaying={isPlaying}
         activeSong={activeSong}
-        // handlePlayClick={() => handlePlayClick(song, i)}
+        song={song}
+        handlePause={handlePauseClick}
+        handlePlay={handlePlayClick}
       />
     </div>
   );
